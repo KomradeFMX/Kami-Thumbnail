4e71f3407a5e611501b22c764d712c3667974f38
KomradeFMX <fm235mh@gmail.com>
2023-03-21 13:01:07 +0100

    Basic invalid link detector

--- js/logic.js
+++ js/logic.js
@@ -2,9 +2,12 @@ function fetchThumbnail() {
     url = document.getElementById("input").value;
     div = document.getElementById("thumbnails");
 
+
     highRes = `https://img.youtube.com/vi/${url.substring(url.lastIndexOf("=") + 1, url.length)}/hqdefault.jpg`
     standardRes = `https://img.youtube.com/vi/${url.substring(url.lastIndexOf("=") + 1, url.length)}/mqdefault.jpg`
     lowRes = `https://img.youtube.com/vi/${url.substring(url.lastIndexOf("=") + 1, url.length)}/default.jpg`
+
+    if (!url.includes("youtube") && url == "" ) return alert("That doesn´t look like a valid youtube link!");
 
     div.innerHTML = `<details open="">
                         <summary>High Resolution</summary>
